59a609d7d4f3b1a1a697a5397c0c2ed539c08c1a
Matilda Fahlström <matildafahlstrom90@hotmail.com>
2026-05-02 12:07:34 +0200

    Writing install and run scripts

--- build_run.sh
+++ build_run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+CONTAINER_NAME="tomcat-container"
+WAR_FILE="boardgame-club-administration.war"
 
 mvn clean package
 
-docker cp target/boardgame-club-administration.war tomcat-container:/usr/local/tomcat/webapps/
+if ! docker ps | grep -q "$CONTAINER_NAME"; then
+    docker start "$CONTAINER_NAME"
+fi
 
-docker restart tomcat-container
+docker cp target/"$WAR_FILE" "$CONTAINER_NAME":/usr/local/tomcat/webapps/
+docker restart "$CONTAINER_NAME"
