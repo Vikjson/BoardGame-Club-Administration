b29231fe611384c54229e851a7b7a3ac17b9630c
Matilda Fahlström <matildafahlstrom90@hotmail.com>
2026-05-04 19:08:53 +0200

    Implementing getAll and error handling in getById in MemberController

--- src/main/java/se/yrgo/controller/MemberController.java
+++ src/main/java/se/yrgo/controller/MemberController.java
@@ -1,12 +1,12 @@
 package se.yrgo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import se.yrgo.domain.Member;
+import se.yrgo.error.MemberNotFoundException;
 import se.yrgo.service.MemberService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/members")
@@ -17,17 +17,29 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
-
-//    @GetMapping
-//    public String getAll() {
-//        try {
 //
-//        }
+//    @PostMapping
+//    public void saveMember(Member member){
+//
 //    }
 
+    @GetMapping
+    public List<Member> getAll() {
+        return memberService.getAllClubMembers();
+    }
+
     @GetMapping("/{id}")
-    public String getById(@PathVariable int id) {
-        Member member = memberService.getById(id);
-        return member.getName();
+    public Member getById(@PathVariable int id) {
+        try {
+            Member member = memberService.getById(id);
+            return member;
+        }
+        catch (MemberNotFoundException e) {
+            System.err.println(e.getMessage());
+            throw new MemberNotFoundException(e);
+        }
+        catch (Exception e) {
+            throw new RuntimeException("There was an internal server error.");
+        }
     }
 }
