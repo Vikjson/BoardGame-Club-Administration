f2cef655f0afce9150e9eb41f03adb6bb30077e1
HangPhan <hangphan0508@gmail.com>
2026-05-09 01:32:47 +0200

    complete Game Entity

--- src/main/java/se/yrgo/domain/Game.java
+++ src/main/java/se/yrgo/domain/Game.java
@@ -1,9 +1,17 @@
 package se.yrgo.domain;
 
 import jakarta.persistence.*;
+import org.jspecify.annotations.NonNull;
 
 import java.time.LocalDate;
 import java.util.List;
+
+/**
+ * Represents a board game owned by the club library.
+ * <p>
+ * A game entity stores information about games all
+ * can be used in game session in the game sessions by members.
+ */
 
 @Entity
 public class Game {
@@ -11,7 +19,9 @@ public class Game {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int gameId;
 
+    @Column(nullable = false)
     private String gameName;
+    @Column(nullable = false)
     private LocalDate purchaseDate;
     private int totalPlayers;
     private String category;
@@ -20,16 +30,66 @@ public class Game {
     private String description;
     private String memberComment;
 
+    /**
+     * Default constructor required by JPA
+     */
     public Game() {
     }
 
-    public Game(int gameId, String gameName, LocalDate purchaseDate, int totalPlayers, String category, int recommendedAge, String description, String memberComment) {
+    /**
+     * Creates a complete Game object including its database identifier.
+     *
+     * @param gameId          unique identifier of the game
+     * @param gameName        name of the board game
+     * @param purchaseDate    date when the game was purchased
+     * @param totalPlayers    maximum number of supported players
+     * @param category        game category of genre
+     * @param recommendedAge  recommended minimum player age
+     * @param averagePlayTime average duration of gameplay in minutes
+     * @param description     description of the game
+     * @param memberComment   member comments, reviews, recommends about the game
+     *
+     */
+    public Game(int gameId, @NonNull String gameName, @NonNull LocalDate purchaseDate, int totalPlayers, String category, int recommendedAge, int averagePlayTime, String description, String memberComment) {
+        if (recommendedAge < 0 || averagePlayTime < 0 || description.isEmpty()) {
+            throw new IllegalArgumentException("recommendedAge or averagePlayTime cannot be negative");
+        }
+        if (gameName.isEmpty()) {
+            throw new IllegalArgumentException("Game name cannot be empty");
+        }
         this.gameId = gameId;
         this.gameName = gameName;
         this.purchaseDate = purchaseDate;
         this.totalPlayers = totalPlayers;
         this.category = category;
         this.recommendedAge = recommendedAge;
+        this.averagePlayTime = averagePlayTime;
+        this.description = description;
+        this.memberComment = memberComment;
+    }
+
+    /**
+     * Creates a new Game object without a database identifier.
+     * <p>
+     * This constructor is typically used when creating a new game before it is persisted to the database.
+     *
+     * @param gameName        name of the board game
+     * @param purchaseDate    date when the game was purchased
+     * @param totalPlayers    maximum number of supported players
+     * @param category        game category of genre
+     * @param recommendedAge  recommended minimum player age
+     * @param averagePlayTime average duration of gameplay in minutes
+     * @param description     description of the game
+     * @param memberComment   member comments, reviews, recommends about the game
+     *
+     */
+    public Game(@NonNull String gameName, @NonNull LocalDate purchaseDate, int totalPlayers, String category, int recommendedAge, int averagePlayTime, String description, String memberComment) {
+        this.gameName = gameName;
+        this.purchaseDate = purchaseDate;
+        this.totalPlayers = totalPlayers;
+        this.category = category;
+        this.recommendedAge = recommendedAge;
+        this.averagePlayTime = averagePlayTime;
         this.description = description;
         this.memberComment = memberComment;
     }
@@ -38,6 +98,15 @@ public class Game {
         return gameName;
     }
 
-//    @OneToMany(mappedBy = "game")
-//    private List<GameSession> sessions;
+    public int getGameId() {
+        return gameId;
+    }
+
+    public int getRecommendedAge() {
+        return recommendedAge;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
