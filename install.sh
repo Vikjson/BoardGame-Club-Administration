f847d1d98a974daa796804a97a3cec65f6d6a8a7
Matilda Fahlström <matildafahlstrom90@hotmail.com>
2026-05-11 14:17:52 +0200

    Updating scripts

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ DB_IMAGE="mcr.microsoft.com/mssql/server"
 DB_IMAGE_WITH_VERSION="mcr.microsoft.com/mssql/server:2019-latest"
 MSSQL_CONTAINER_NAME="mssql-boardgames"
 DB_PASSWORD="yrgoP4ssword"
+SQL_SCRIPT="boardGameClub_TestDB.sql"
 
 if ! docker images | grep -q "$DB_IMAGE"; then
 echo "Pulling SQL Server docker image"
@@ -19,7 +20,13 @@ fi
 
 docker run -d -p 32772:8080 --name "$TOMCAT_CONTAINER_NAME" tomcat
 
+docker cp "$SQL_SCRIPT" "$MSSQL_CONTAINER_NAME":/usr/
+docker exec -it "$MSSQL_CONTAINER_NAME" "bash"
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P "$DB_PASSWORD" -C -i /usr/"$SQL_SCRIPT";
+
+exit
+
 cd frontend/
 npm install
 
-#TODO: Init DB tables etc?
+#todo script in container?
