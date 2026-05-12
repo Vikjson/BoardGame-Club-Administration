e71e02f116497604af3e69142f1b411b16179c49
Viktoria Johansson <viktoria.k.johansson@gmail.com>
2026-05-12 13:10:24 +0200

    Add aspectJ dependency to Pom and created PerformanceTiming advice class

---
+++ src/main/java/se/yrgo/advice/PerformanceTimingAdvice.java
@@ -0,0 +1,32 @@
+package se.yrgo.advice;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class PerformanceTimingAdvice {
+
+    @Around("execution(* se.yrgo.service..*(..)) || execution(* se.yrgo.data..*(..))")
+    public Object measureMethodTime(ProceedingJoinPoint pjp) throws Throwable {
+
+        long start = System.currentTimeMillis();
+
+        try {
+            return pjp.proceed();
+        } finally {
+            long end = System.currentTimeMillis();
+            long time = end - start;
+
+            System.out.println(
+                    pjp.getTarget().getClass().getSimpleName()
+                            + "."
+                            + pjp.getSignature().getName()
+                            + " took "
+                            + time
+                            + " ms"
+            );
+        }
+    }
+}
+
